52f1c75c824e7f805eef6fa54c08fc8c5cd6a1e2
bhtylerbell <bhtylerbell@gmail.com>
2025-01-04 10:37:48 -0600

    Added code comments

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    characters = get_character_count(text)
-    sorted_characters = sort_character_count(characters)
+def main(book_path):
+    text = get_book_text(book_path) # Open and read the text for the input book
+    num_words = get_num_words(text) # Get number of words found in the book not including whitespace
+    characters = get_character_count(text) # Get count of indiviual letters not including special characters
+    sorted_characters = sort_character_count(characters) # Sort characters alphabetically
     print(f"--- Begin Report on {book_path} ---")
     print(f"There are {num_words} words found in this document")
     print("")
@@ -39,4 +38,4 @@ def sort_character_count(chacter_count):
     sorted_count = {key: chacter_count[key] for key in sorted(chacter_count)}
     return sorted_count
     
-main()
+main("books/frankenstein.txt")
